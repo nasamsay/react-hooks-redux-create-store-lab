dc94cdaabd1974ded43f64ba3cda971016e780b4
Nasam Sayour <nasamsay@gmail.com>
2022-10-03 09:10:24 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 // Note: createStore and candyReducer must be exported for the tests to run
-
-export function createStore() {
+let state;
+export function createStore(reducer) {
   // write your createStore code here
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+  function getState() {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState,
+  }
 }
 
 export function candyReducer(state = [], action) {
@@ -24,3 +36,4 @@ function render() {
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+let store = createStore(candyReducer([], {type:"candies/add", candy : "" }))
